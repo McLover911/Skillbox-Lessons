0da32a45b834d217e9acb6544c50c02c1d9b841f
McLover911 <nikitos-dr@mail.ru>
2021-12-25 19:01:02 +0300

    Lesson 2

---
+++ LESSON-2/TASK-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Кузьменко Станислав Юрьевич";
+            int age = 23;
+            string email = "kust@gmail.com";
+            float programmingScores = 73.5f;
+            float mathScores = 85.2f;
+            float physicsScores = 77.9f;
+
+            string pattern = "ФИО: {0}" +
+                             "\nВозраст: {1}" +
+                             "\nEmail: {2}" +
+                             "\nБаллы по программированию: {3}" +
+                             "\nБаллы по математике: {4}" +
+                             "\nБаллы по физике: {5}";
+
+            Console.WriteLine(pattern,
+                              fullName,
+                              age,
+                              email,
+                              programmingScores,
+                              mathScores,
+                              physicsScores);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LESSON-2/TASK-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float programmingScores = 73.5f;
+            float mathScores = 85.2f;
+            float physicsScores = 77.9f;
+
+            float scoreSum;
+            float scoreMean;
+
+            scoreSum = programmingScores + mathScores + physicsScores;
+            scoreMean = scoreSum / 3;
+
+            string scoreMeanFormated = scoreMean.ToString("#.##");
+
+            Console.WriteLine($"Сумма баллов: {scoreSum} \nСреднее арифметическое баллов: {scoreMeanFormated}");
+
+            Console.ReadKey();
+        }
+    }
+}
