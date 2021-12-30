ef255a72032f559dd45bdfaf4f324f9e9333f030
McLover911 <56025633+McLover911@users.noreply.github.com>
2021-12-30 22:03:37 +0300

    Lesson 3

---
+++ LESSON-3/TASK-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TASK_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Введите целое число:");
+
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Данное число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Данное число нечётное");
+            }
+        }
+    }
+}
---
+++ LESSON-3/TASK-2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TASK_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfCards;
+            int score = 0;
+            string cardType;
+
+            Console.WriteLine("Введите количество карт на руках:");
+
+            amountOfCards = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < amountOfCards; i++)
+            {
+                Console.WriteLine($"Введите номинал {i + 1}-й карты:");
+
+                cardType = Console.ReadLine().ToUpper();
+
+                switch (cardType)
+                {
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                        score += int.Parse(cardType);
+                        break;
+                    case "10":
+                    case "J":
+                    case "Q":
+                    case "K":
+                    case "T":
+                        score += 10;
+                        break;
+                    default:
+                        Console.WriteLine("Несуществующий номинал");
+                        break;
+                }
+
+            }
+
+            Console.WriteLine($"Сумма карт равна {score}");
+        }
+    }
+}
