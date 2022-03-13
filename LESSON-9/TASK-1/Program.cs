b4a2627757ec39db67f80487be7a61fcbd98faab
McLover911 <nikitos-dr@mail.ru>
2022-03-14 00:37:01 +0300

    Lesson 9

--- LESSON-9/TASK-1/Program.cs
+++ LESSON-9/TASK-1/Program.cs
@@ -1,89 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using Newtonsoft.Json.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-using Telegram.Bot.Extensions;
-using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-namespace Example_931
+string token = System.IO.File.ReadAllText(@"E:\TelegramBots\1\TheFirstPrototypeBot.txt");
+TelegramBotClient botClient = new TelegramBotClient(token);
+
+using var cts = new CancellationTokenSource();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+var receiverOptions = new ReceiverOptions
 {
-    class Program
+    AllowedUpdates = { } // receive all update types
+};
+
+botClient.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cancellationToken: cts.Token);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    var chatId = update.Message.Chat.Id;
+    var messageText = update.Message.Text;
+    string creationDate = Convert.ToString(DateTime.Now).Replace(':', '-');
+
+
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Type != UpdateType.Message)
     {
-        static TelegramBotClient bot;
+        return;
+    }
 
-        static void Main(string[] args)
+    int update_id = 0;
+
+    while (true)
+    {
+        var msgs = botClient.GetUpdatesAsync(update_id).Result;
+
+        foreach (dynamic msg in msgs)
         {
+            update_id = Convert.ToInt32(msg.Id) + 1;
+            string userMessage = msg.Message.Text;
+            string userId = Convert.ToString(msg.Message.From.Id);
+            string useFirstrName = msg.Message.From.FirstName;
+            string text = $"{useFirstrName} {userId} {userMessage} {msg.Id} {msg.Message.Type} {update_id}";
 
-            string token = File.ReadAllText("5222034773:AAGfn4mglD34dbQ7xmHtYmk-PrgBjrSwNAE");
+            //if (msg.Message.Type == MessageType.Voice)
+            //{
+            //    Download(msg.Message.Voice.FileId, creationDate + msg.Message.Voice.FileId);
+            //}
 
-            #region exc
+            //if (msg.Message.Type == MessageType.Photo)
+            //{
+            //    string photoFileID = msg.Message.Photo[msg.Message.Photo.Length - 1].FileId;
 
-            //// https://hidemyna.me/ru/proxy-list/?maxtime=250#list
+            //    Download(photoFileID, "Картинка " + creationDate + ".png");
+            //}
 
-            // Содержит параметры HTTP-прокси для System.Net.WebRequest класса.
-            var proxy = new WebProxy()
+            if (msg.Message.Text == "/start")
             {
-                Address = new Uri($"http://77.87.240.74:3128"),
-                UseDefaultCredentials = false,
-                //Credentials = new NetworkCredential(userName: "login", password: "password")
-            };
-
-            // Создает экземпляр класса System.Net.Http.HttpClientHandler.
-            var httpClientHandler = new HttpClientHandler() { Proxy = proxy };
-
-            // Предоставляет базовый класс для отправки HTTP-запросов и получения HTTP-ответов 
-            // от ресурса с заданным URI.
-            HttpClient hc = new HttpClient(httpClientHandler);
-
-            bot = new TelegramBotClient(token, hc);
-
-            #endregion
-
-            //bot = new TelegramBotClient(token);
-            bot.OnMessage += MessageListener;
-            bot.StartReceiving();
-            Console.ReadKey();
-        }
-
-        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
-        {
-            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
-
-            Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
-
-
-            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
-            {
-                Console.WriteLine(e.Message.Document.FileId);
-                Console.WriteLine(e.Message.Document.FileName);
-                Console.WriteLine(e.Message.Document.FileSize);
-
-                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+                Message message = await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "Бот готов к работе",
+                    disableNotification: true,
+                    cancellationToken: cancellationToken);
             }
 
-            if (e.Message.Text == null) return;
-
-            var messageText = e.Message.Text;
-
-
-            bot.SendTextMessageAsync(e.Message.Chat.Id,
-                $"{messageText}"
-                );
+            Console.WriteLine(text);
         }
 
-        static async void DownLoad(string fileId, string path)
-        {
-            var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream("_" + path, FileMode.Create);
-            await bot.DownloadFileAsync(file.FilePath, fs);
-            fs.Close();
 
-            fs.Dispose();
-        }
+
+        Thread.Sleep(100);
+    }
+
+}
+
+Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
+async void Download(string fileId, string path)
+{
+    var file = await botClient.GetFileAsync(fileId);
+
+    using (FileStream fs = new FileStream(path, FileMode.Create))
+    {
+        await botClient.DownloadFileAsync(file.FilePath, fs);
     }
 }
