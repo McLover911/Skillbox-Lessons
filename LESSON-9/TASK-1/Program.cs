aa77a8e93a89f84af464770a3d55fc687767120a
McLover911 <nikitos-dr@mail.ru>
2022-04-06 21:15:49 +0300

    Lesson 9 fixed

--- LESSON-9/TASK-1/Program.cs
+++ LESSON-9/TASK-1/Program.cs
@@ -13,7 +13,7 @@ using var cts = new CancellationTokenSource();
 
 Dictionary<int, string> fileIDs = new Dictionary<int, string>();
 
-string token = System.IO.File.ReadAllText(@"E:\TelegramBots\1\TheFirstPrototypeBot.txt");
+string token = System.IO.File.ReadAllText(@"E:\Dev\TelegramBots\1\TheFirstPrototypeBot.txt");
 TelegramBotClient botClient = new TelegramBotClient(token);
 
 var receiverOptions = new ReceiverOptions
@@ -43,91 +43,102 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
     string messageText = update.Message.Text;
     string creationDate = Convert.ToString(DateTime.Now).Replace(':', '-').Replace(' ', '_');
 
-    string savePath = @"E:\Skillbox\C#\Skillbox-Lessons\LESSON-9\TASK-1\bin\Debug\net6.0\Save\";
-    string downloadPath = @"E:\Skillbox\C#\Skillbox-Lessons\LESSON-9\TASK-1\bin\Debug\net6.0\Download\";
+    string savePath = @"Save";
+    string downloadPath = @"Download";
 
     if (update.Type != UpdateType.Message)
     {
         return;
     }
 
-    if (update.Message.Type == MessageType.Voice)
+    switch (update.Message.Text)
     {
-        Download(update.Message.Voice.FileId, savePath + creationDate + update.Message.Voice.FileId);
-    }
-
-    if (update.Message.Type == MessageType.Photo)
-    {
-        string photoFileID = update.Message.Photo[update.Message.Photo.Length - 1].FileId;
-
-        Download(photoFileID, savePath + "Картинка_" + creationDate + ".png");
-    }
-
-    if (update.Message.Text == "/start")
-    {
-        Message message = await botClient.SendTextMessageAsync(
+        case "/start":
+            Message message = await botClient.SendTextMessageAsync(
             chatId: chatId,
             text: "Бот готов к работе",
             disableNotification: true,
             cancellationToken: cancellationToken);
-    }
 
-    if (update.Message.Text == "/getfiles")
-    {
-        DirectoryInfo directory = new DirectoryInfo(savePath);
-        FileInfo[] files = directory.GetFiles();
-        int fileNum = 0;
+            break;
 
-        foreach (FileInfo file in files)
-        {
-            FileStream fs = System.IO.File.OpenRead(file.FullName);
-            InputOnlineFile iof = new InputOnlineFile(fs);
-            iof.FileName = file.Name;
+        case "/getfiles":
+            DirectoryInfo directory = new DirectoryInfo(savePath);
+            FileInfo[] files = directory.GetFiles();
+            int fileNum = 0;
+
+            foreach (FileInfo file in files)
+            {
+                FileStream fs = System.IO.File.OpenRead(file.FullName);
+                InputOnlineFile iof = new InputOnlineFile(fs);
+                iof.FileName = file.Name;
+
+                await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: $"Номер файла: {fileNum}",
+                    cancellationToken: cancellationToken);
+
+                var sendDoc = await botClient.SendDocumentAsync(update.Message.Chat.Id, iof, file.Name);
+                fileIDs.Add(fileNum, sendDoc.Document.FileId);
+
+                fileNum++;
+            }
 
             await botClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: $"Номер файла: {fileNum}",
-                cancellationToken: cancellationToken);
-
-            var sendDoc = await botClient.SendDocumentAsync(update.Message.Chat.Id, iof, file.Name);
-            fileIDs.Add(fileNum, sendDoc.Document.FileId);
-            
-            fileNum++;
-
-            Thread.Sleep(300);
-        }
-
-        await botClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: $"Чтобы скачать файл, введите команду /get с номером файла через пробел",
-                cancellationToken: cancellationToken);
+                    chatId: chatId,
+                    text: $"Чтобы скачать файл, введите команду /get с номером файла через пробел",
+                    cancellationToken: cancellationToken);
+            break;
     }
 
-    if (update.Message.Type == MessageType.Text)
+    switch (update.Message.Type)
     {
-        messageSubstrings = update.Message.Text.Split(' ');
+        case MessageType.Voice:
+            Download(update.Message.Voice.FileId, savePath + creationDate + update.Message.Voice.FileId);
+            break;
 
-        if (messageSubstrings[0] == "/get")
-        {
-            int fileIndex = Convert.ToInt32(messageSubstrings[1]);
+        case MessageType.Photo:
+            string photoFileID = update.Message.Photo[update.Message.Photo.Length - 1].FileId;
+            Download(photoFileID, savePath + "Картинка_" + creationDate + ".png");
+            break;
 
-            if (fileIDs.ContainsKey(fileIndex))
+        case MessageType.Text:
+            messageSubstrings = update.Message.Text.Split(' ');
+            if (messageSubstrings[0] == "/get")
             {
-                DirectoryInfo directory = new DirectoryInfo(savePath);
-                FileInfo[] files = directory.GetFiles();
-                FileStream fs = System.IO.File.OpenRead(files[fileIndex].FullName);
+                int fileIndex = Convert.ToInt32(messageSubstrings[1]);
 
-                Download(fileIDs[fileIndex], downloadPath + "Файл_" + creationDate);
-            }
-            else
-            {
+                if (fileIDs.ContainsKey(fileIndex))
+                {
+                    DirectoryInfo directory = new DirectoryInfo(savePath);
+                    FileInfo[] files = directory.GetFiles();
+                    FileStream fs = System.IO.File.OpenRead(files[fileIndex].FullName);
+
+                    Download(fileIDs[fileIndex], downloadPath + "Файл_" + creationDate);
+                }
+                else
+                {
+                    await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "Нет такого номера",
+                    cancellationToken: cancellationToken);
+                }
+
                 await botClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: $"Нет такого номера",
-                cancellationToken: cancellationToken);
+                    chatId: chatId,
+                    text: "Файл скачен",
+                    cancellationToken: cancellationToken);
             }
-        }
+            break;
+
+        default:
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: $"Unknown type",
+                cancellationToken: cancellationToken);
+            break;
     }
+
 }
 
 Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
